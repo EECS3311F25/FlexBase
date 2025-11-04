f735293fc683d6c904bd907ce74b49dcaefd551e
Pranay Tavanam <pranay.271@gmail.com>
2025-11-03 21:35:29 -0500

    minor change in db input
    added hardcoded entry to db input for testing

--- doc/sprint0/FlexBase-Tracker/src/main/java/Controller/DatabaseController.java
+++ doc/sprint0/FlexBase-Tracker/src/main/java/Controller/DatabaseController.java
@@ -17,16 +17,21 @@ public class DatabaseController {
 			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
 		}
 		
-		String input = "insert into habit values"
+		String input1 = "insert into habit values"
 				 + "('1', 'gaming', '3', '19:00:00', '21:00:00');"
 				 ;
+		
+//		String input2 = "insert into habit values"
+//				 + "('2', 'studying', '8', '12:00:00', '18:00:00');"
+//				 ;
+		
 		String select = "select * from habit";
 		
 		
 		try {
 			Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
 			
-			PreparedStatement stmt = con.prepareStatement(input);
+			PreparedStatement stmt = con.prepareStatement(input1);
 //			stmt.executeUpdate();
 			
 			stmt = con.prepareStatement(select);
