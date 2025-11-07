d6de6ed5dcd173ebb33b96f2632cfc59bbe3ea55
Pranay Tavanam <pranay.271@gmail.com>
2025-11-07 17:04:01 -0500

    temp database stuff

--- Database/FlexBase_Schema.sql
+++ Database/FlexBase_Schema.sql
@@ -6,10 +6,17 @@
 # create HABIT table to store all attributes of user's habits
 create table HABIT (
  HABIT_ID int not null auto_increment primary key,
+ #USER_ID char(50) not null,
  HABIT_NAME char(50) not null,
  HABIT_PRIORITY int not null,
  HABIT_TIME_START time,
  HABIT_TIME_END time
+ #foreign key USER_ID references (USER_INFO) USER_ID
  );
  
- 
+#create table USER_INFO (
+#USER_ID int not null auto_increment primary key,
+#USER_NAME char(50) not null,
+#USER_PASS char(50) not null
+#);
+
