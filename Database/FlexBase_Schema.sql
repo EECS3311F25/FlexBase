8ced557393e2fc409562305c61b662407f6ca49a
Pranay Tavanam <pranay.271@gmail.com>
2025-11-17 18:57:58 -0500

    touch up on database creation
    refactored SQL for habit, user_info and optimized_habit

--- Database/FlexBase_Schema.sql
+++ Database/FlexBase_Schema.sql
@@ -1,7 +1,15 @@
 ## commented test code to verify table behavior
 	#show tables from flexbase_schema;
+    
+    #describe user_info;
 	#describe habit;
+    #describe optimized_habit;
+    
 	#select * from habit;
+    #select * from user_info;
+    #select * from optimized_habit;
+    
+    #drop table habit, user_info;
 
 # create USER_INFO table to store user information
 create table USER_INFO (
@@ -13,24 +21,24 @@ USER_PASS char(50) not null
 # create HABIT table to store all attributes of user's habits
 create table HABIT (
  HABIT_ID int not null auto_increment primary key,
- USER_ID char(50) not null,
+ USER_ID int not null,
  HABIT_NAME char(50) not null,
  HABIT_PRIORITY int not null,
  HABIT_TIME_START time,
- HABIT_TIME_END time
- #foreign key (USER_ID) references USER_INFO(USER_ID)
+ HABIT_TIME_END time,
+ foreign key (USER_ID) references USER_INFO(USER_ID)
  );
  
  # create OPTIMIZED_HABIT table to store optimized habits and their attributes
  create table OPTIMIZED_HABIT (
  O_HABIT_ID int not null auto_increment primary key,
- USER_ID char(50) not null,
+ USER_ID int not null,
  O_HABIT_NAME char(50) not null,
  O_HABIT_PRIORITY int not null,
  O_HABIT_TIME_START time,
  O_HABIT_TIME_END time,
  foreign key (O_HABIT_ID) references HABIT(HABIT_ID),
- foreign key (USER_ID) references USER(USER_ID)
+ foreign key (USER_ID) references USER_INFO(USER_ID)
  );
 
 
